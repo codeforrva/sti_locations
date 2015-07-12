52cff3f9a9902808b4e4dcd99b4df188b15eeba0
adamhake <adamhake@gmail.com>
2015-07-12 16:19:25 -0400

    adds body parser.

--- app.js
+++ app.js
@@ -24,7 +24,7 @@ app.get('/', function (req, res){
 });
 
 app.post('/', function (req, res){
-  console.log(req.url.query);
+  console.log(req.body);
 });
 
 var server = app.listen(process.env.PORT || 3000, function () {
