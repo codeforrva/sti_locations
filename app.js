e2da280e7d03eee744703875ef3cab9101de5640
adamhake <adamhake@gmail.com>
2015-07-12 20:56:38 -0400

    adds functionality.

--- app.js
+++ app.js
@@ -1,31 +1,18 @@
 var ACCOUNT_SID = 'ACaf0232e2832b0cbe15d9c74fc812cf7e';
 var AUTH_TOKEN = '8b9c7db15691ed32283ccb89acc7ac99';
 var twilio = require('twilio')(ACCOUNT_SID, AUTH_TOKEN);
-var twilio_response = require('twilio');
-var resp = new twilio_response.TwimlResponse();
 var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
+var sti = require('./src/sti');
 
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
-app.get('/', function (req, res){
-  twilio.sendMessage({
-    to: '+17174870605',
-    from: '+1804298-7609',
-    body: 'Test Message from node!'
-  }, function(err, responseData){
-    if (!err) { // "err" is an error received during the request, if any
-      console.log(responseData.from); // outputs "+14506667788"
-      console.log(responseData.body); // outputs "word to your mother."
-    }
-  });
-  res.send('Greetings!');
-});
 
-app.post('/', function (req, res){
-  console.log(req.body);
-});
+app.use(bodyParser.json());
+
+app.get('/', sti.validate_zip, sti.get_locations, sti.send_locations);
+
+app.post('/', sti.validate_zip, sti.get_locations, sti.send_locations);
 
 var server = app.listen(process.env.PORT || 3000, function () {
   console.log('Code For RVA STI SMS App Started!');
