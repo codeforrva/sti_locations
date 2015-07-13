f6b2bf8a845ecaef0ad07ba2f8ddf94fe3329670
adamhake <adamhake@gmail.com>
2015-07-12 21:50:49 -0400

    adds new relic.

---
+++ newrelic.js
@@ -0,0 +1,24 @@
+/**
+ * New Relic agent configuration.
+ *
+ * See lib/config.defaults.js in the agent distribution for a more complete
+ * description of configuration variables and their potential values.
+ */
+exports.config = {
+  /**
+   * Array of application names.
+   */
+  app_name: ['STI-APP'],
+  /**
+   * Your New Relic license key.
+   */
+  license_key: '852dd7757046d5490aadc8bb395a5b5e452cb2bf',
+  logging: {
+    /**
+     * Level at which to log. 'trace' is most useful to New Relic when diagnosing
+     * issues with the agent, 'info' and higher will impose the least overhead on
+     * production applications.
+     */
+    level: 'info'
+  }
+}
